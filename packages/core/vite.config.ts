8cad42381d4d1f59e75e7a1b38f67b86b256ec8e
AbigailDeng <Abigail.deng@ienyan.com>
2023-11-09 11:04:42 +0800

    feat: fix build

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,15 +2,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    svgr()
-  ],
+  plugins: [react(), tsconfigPaths(), svgr()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.tsx'),
+      name: 'ReactComponents',
+      fileName: (format) => `react-components.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'react',
+          'react-dom': 'react-dom'
+        }
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
