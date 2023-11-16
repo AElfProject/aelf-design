b3f5171092cde54832c280d35684b2b2c6b29c03
-g <xiyang.liu@aelf.io>
2023-11-16 14:59:47 +0800

    feat: vite config
    vite config

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 import { resolve } from 'path'
+import pkg from './package.json'
+// import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import rollupResolve from '@rollup/plugin-node-resolve'
+import dts from 'vite-plugin-dts'
 
 import { defaultTemplate } from './template'
 
@@ -12,23 +17,48 @@ export default defineConfig({
   build: {
     emptyOutDir: false,
     lib: {
-      entry: resolve(__dirname, 'src/components/index.tsx'),
-      name: 'ReactComponents',
-      fileName: (format) => `react-components.${format}.js`
+      entry: resolve(__dirname, 'src/components/index.tsx')
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'react',
-          'react-dom': 'react-dom'
+      output: [
+        {
+          dir: resolve(__dirname, 'dist/umd'),
+          format: 'umd',
+          name: pkg.name,
+          sourcemap: true,
+          globals: {
+            react: 'react',
+            'react-dom': 'react-dom'
+          }
+        },
+        {
+          dir: resolve(__dirname, 'dist/es'),
+          format: 'es',
+          sourcemap: true
+        },
+        {
+          dir: resolve(__dirname, 'dist/cjs'),
+          format: 'cjs',
+          sourcemap: true
         }
-      }
+      ]
     }
   },
   plugins: [
+    commonjs(),
     react(),
     tsconfigPaths(),
+    dts({
+      insertTypesEntry: true,
+      copyDtsFiles: false
+    }),
+    rollupResolve(),
+    // typescript({
+    //   rollupCommonJSResolveHack: true,
+    //   exclude: ['**/__tests__/**', '**/*.stories.*'],
+    //   clean: true
+    // }),
     svgr({
       svgrOptions: {
         // if want to use svgoConfig, you need these two plugins first
