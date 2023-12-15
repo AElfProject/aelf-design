bf1067a17adfb65754bdea34daf2cc138abba03e
Peterbjx <jianxiong.bai@aelf.io>
2023-12-15 17:14:31 +0800

    feat: add reactDocgenTypescriptOptions

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite'
-
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -12,9 +11,22 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {}
   },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      // 👇 Default prop filter, which excludes props from node_modules
+      propFilter: (prop) => {
+        const regexString =
+          '(?:\\/node_modules\\/(?:rc(?:-[^\\/]+)?|antd)|\\/src\\/components)\\/'
+        const regex = new RegExp(regexString)
+        return prop.parent ? regex.test(prop.parent.fileName) : true
+      }
+    }
+  },
   docs: {
     autodocs: 'tag',
-    defaultName: 'Overview',
+    defaultName: 'Overview'
   }
 }
 export default config
