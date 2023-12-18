ee1b3d4689cf90131c463e49f36f534c58b57173
AbigailDeng <Abigail.deng@ienyan.com>
2023-12-18 16:48:24 +0800

    feat: input story

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -1,16 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false
-      },
-      propFilter: () => true
-    }
-  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -24,14 +14,13 @@ const config: StorybookConfig = {
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
+      // prevent showing undefined
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false
+      },
       shouldExtractLiteralValuesFromEnum: true,
-      // 👇 Default prop filter, which excludes props from node_modules
-      propFilter: (prop) => {
-        const regexString =
-          '(?:\\/node_modules\\/(?:rc(?:-[^\\/]+)?|antd)|\\/src\\/components)\\/'
-        const regex = new RegExp(regexString)
-        return prop.parent ? regex.test(prop.parent.fileName) : true
-      }
+      propFilter: (prop) => true
     }
   },
   docs: {
--- packages/core/src/components/Input/index.stories.tsx
+++ packages/core/src/components/Input/index.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import AelfdInput from './index'
@@ -18,12 +19,20 @@ export default meta
 type Story = StoryObj<typeof AelfdInput>
 // // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
-  // args: {
-  //   addonAfter: 'AELF',
-  //   addonBefore: 'ELF',
-  //   placeholder: 'Basic usage'
-  // },
+  args: {
+    addonAfter: 'AELF',
+    addonBefore: 'ELF'
+  },
   parameters: {
     layout: 'padded'
   }
+}
+type TextAreaStory = StoryObj<typeof AelfdInput.TextArea>
+export const TextArea: TextAreaStory = {
+  render: () => <AelfdInput.TextArea />
+}
+
+type PasswordStory = StoryObj<typeof AelfdInput.Password>
+export const Password: PasswordStory = {
+  render: () => <AelfdInput.Password />
 }
