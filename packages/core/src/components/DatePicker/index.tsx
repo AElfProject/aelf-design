6fceca6ddafc237a6b51a68b347998b985e43ef2
aelf-lxy <xiyang.liu@aelf.io>
2024-01-11 18:37:08 +0800

    test

--- packages/core/src/components/DatePicker/index.tsx
+++ packages/core/src/components/DatePicker/index.tsx
@@ -2,35 +2,15 @@ import {
   DatePicker as AntdDatePicker,
   DatePickerProps as AntdDatePickerProps
 } from 'antd'
-import {
-  DatePicker as AdmDatePicker,
-  DatePickerProps as AdmDatePickerProps
-} from 'antd-mobile'
+
 import useStyles from './style'
 
 export type TDatePickerPropsForPC = AntdDatePickerProps
-export type TDatePickerPropsForMobile = AdmDatePickerProps
 
 function DatePickerForPC(props: TDatePickerPropsForPC) {
   return <AntdDatePicker {...props} />
 }
 
-function DatePickerForMobile({
-  confirmText = 'Done',
-  cancelText = 'Cancel',
-  ...rest
-}: TDatePickerPropsForMobile) {
-  const { styles: st } = useStyles()
-  return (
-    <AdmDatePicker
-      {...rest}
-      confirmText={confirmText}
-      cancelText={cancelText}
-      className={st.datePickerForMobileWrap}
-    />
-  )
-}
-
 // DatePicker.RangePicker = RangePicker
 
-export { DatePickerForPC, DatePickerForMobile }
+export { DatePickerForPC }
