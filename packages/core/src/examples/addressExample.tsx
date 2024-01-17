424551e5743e84f298276c06ff90f3a59fae1f41
Peterbjx <jianxiong.bai@aelf.io>
2024-01-17 18:26:59 +0800

    feat: carousel

--- packages/core/src/examples/addressExample.tsx
+++ packages/core/src/examples/addressExample.tsx
@@ -5,7 +5,7 @@ export default function AddressExample() {
     <>
       <Address
         className="w-[300px]"
-        address="iCQmBkK5m4FC6DMjkMGAxd4q3niWfvTN6DqWgU7BYSGCRMAdDr"
+        address="iCQmBkK5m4FC6DMjkMGAx4q3niWfvTN6DqWgU7BYSGCRMAdDr"
         size="small"
         addressClickCallback={(o, f) => {
           console.log(o, f)
