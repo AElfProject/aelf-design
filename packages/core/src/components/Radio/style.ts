b82b27c1fb2722e6739f0c8754c1b3e9135d0d67
Peterbjx <jianxiong.bai@aelf.io>
2024-01-17 13:42:51 +0800

    feat: change radio style and change upload

--- packages/core/src/components/Radio/style.ts
+++ packages/core/src/components/Radio/style.ts
@@ -4,12 +4,12 @@ export default createStyles(({ css, token, prefixCls }) => {
   return {
     aelfDesignRadio: css`
       &.${prefixCls}-radio-wrapper .${prefixCls}-radio-inner::after {
-        background-color: ${token.colorPrimary};
+        background-color: ${token.customRadio?.dotColor};
       }
       &.${prefixCls}-radio-wrapper
         .${prefixCls}-radio-checked
         .${prefixCls}-radio-inner {
-        background-color: ${token.colorBgContainer};
+        background-color: ${token.colorPrimary};
       }
     `
   }
---
+++ packages/core/src/hooks/useResponsive.ts
@@ -0,0 +1,3 @@
+import { useResponsive } from 'antd-style'
+
+export default useResponsive
